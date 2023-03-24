fd6b168cfccb4b885c3e503847a852319cd36590
pdragoni <paulohbfdf@gmail.com>
2023-03-24 19:16:13 -0300

    [fix] enunciado ex2 - Sequência Fibonacci

--- ex2.js
+++ ex2.js
@@ -1,15 +1,18 @@
-// 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...),
-// escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
+/*
+ 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...),
+escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
 
-// IMPORTANTE:
-// Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
+IMPORTANTE:
+Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
+*/
 
 // Resolução:
 
-const num = parseInt(prompt("Digite um número: ")); // recebe o número a partir da interação do usuário
+// const num = parseInt(prompt("Digite um número: ")); // recebe o número a partir da interação do usuário no navegador
 // const num = 0; // número pré-definido --> teste negativo
 // const num = 20; // número pré-definido --> teste negativo
 // const num = 21; // número pré-definido --> teste positivo
+const num = 34; // número pré-definido --> teste positivo
 
 let a = 0; // primeiro número da sequência
 let b = 1; // segundo número da sequência
@@ -31,3 +34,8 @@ while (c <= num) {
 if (c > num) {
   console.log(num + " não pertence à sequência de Fibonacci.");
 }
+
+/*
+  Resultado:
+  34 pertence à sequência de Fibonacci!
+*/
